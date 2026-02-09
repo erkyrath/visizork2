dff738611d52dbbc280ecbe84063bb79ef128a39
Andrew Plotkin <zarf@ZarfLent.local>
2026-02-08 19:33:16 -0500

    Grab window as any.

--- src/custom/gamedat.ts
+++ src/custom/gamedat.ts
@@ -86,45 +86,47 @@ export const sourcefile_presentation_list: string[] = [
     'crufty.zil',
 ];
 
-export const gamedat_ids = (window as any).gamedat_ids;
+const winany = (window as any);
 
-const sourcefile_capkey_map: SourceFileMap = (window as any).gamedat_sourcefile_capkeymap;
-export const gamedat_sourcefile_keymap: SourceFileMap = (window as any).gamedat_sourcefile_keymap;
-export const gamedat_sourcefile_revkeymap: SourceFileMap = (window as any).gamedat_sourcefile_revkeymap;
-export const gamedat_property_nums = (window as any).gamedat_property_nums as Map<number, PropertyData>;
-export const gamedat_property_names = (window as any).gamedat_property_names as Map<string, PropertyData>;
-export const gamedat_attribute_nums = (window as any).gamedat_attribute_nums as Map<number, AttributeData>;
-export const gamedat_attribute_names = (window as any).gamedat_attribute_names as Map<string, AttributeData>;
-export const gamedat_global_nums = (window as any).gamedat_global_nums as Map<number, GlobalData>;
-export const gamedat_global_names = (window as any).gamedat_global_names as Map<string, GlobalData>;
-export const gamedat_globals_sort_index = (window as any).gamedat_globals_sort_index as GlobalData[];
-export const gamedat_globals_sort_alpha = (window as any).gamedat_globals_sort_alpha as GlobalData[];
-export const gamedat_constant_names = (window as any).gamedat_constant_names as Map<string, ConstantData>;
-export const gamedat_object_ids = (window as any).gamedat_object_ids as Map<number, ObjectData>;
-export const gamedat_object_names = (window as any).gamedat_object_names as Map<string, ObjectData>;
-export const gamedat_object_room_ids = (window as any).gamedat_object_room_ids as Set<number>;
-export const gamedat_object_global_ids = (window as any).gamedat_object_global_ids as Set<number>;
-export const gamedat_object_treesort = (window as any).gamedat_object_treesort as Map<number, number>;
-export const gamedat_string_map = (window as any).gamedat_string_map as Map<number, StringData>;
-export const gamedat_dictword_addrs = (window as any).gamedat_dictword_addrs as Map<number, DictWordData>;
-export const gamedat_dictword_adjs = (window as any).gamedat_dictword_adjs as Map<number, DictWordData>;
-export const gamedat_preposition_nums = (window as any).gamedat_preposition_nums as Map<number, PrepositionData>;
-export const gamedat_grammar_verbnums = (window as any).gamedat_grammar_verbnums as Map<number, GrammarVerbData>;
-export const gamedat_grammar_lines = (window as any).gamedat_grammar_lines as GrammarLineData[];
-export const gamedat_grammar_line_addrs = (window as any).gamedat_grammar_line_addrs as Map<number, GrammarLineData>;
-export const gamedat_grammaractionlines = (window as any).gamedat_grammaractionlines as number[];
-export const gamedat_routine_addrs = (window as any).gamedat_routine_addrs as Map<number, RoutineData>;
-export const gamedat_routine_names = (window as any).gamedat_routine_names as Map<string, RoutineData>;
-export const gamedat_actions = (window as any).gamedat_actions as ActionData[];
-export const gamedat_sourcefiles = (window as any).gamedat_sourcefiles as SourceLinesMap;
-export const gamedat_distances = (window as any).gamedat_distances as AllDistanceMap;
-export const gamedat_roominfo_names = (window as any).gamedat_roominfo_names as Map<string, MapRoom>;
-export const gamedat_commentary = (window as any).gamedat_commentary as CommentaryMap;
-export const gamedat_commentarymap = (window as any).gamedat_commentarymap as CommentaryLineMap;
+export const gamedat_ids = winany.gamedat_ids;
+
+const sourcefile_capkey_map: SourceFileMap = winany.gamedat_sourcefile_capkeymap;
+export const gamedat_sourcefile_keymap: SourceFileMap = winany.gamedat_sourcefile_keymap;
+export const gamedat_sourcefile_revkeymap: SourceFileMap = winany.gamedat_sourcefile_revkeymap;
+export const gamedat_property_nums = winany.gamedat_property_nums as Map<number, PropertyData>;
+export const gamedat_property_names = winany.gamedat_property_names as Map<string, PropertyData>;
+export const gamedat_attribute_nums = winany.gamedat_attribute_nums as Map<number, AttributeData>;
+export const gamedat_attribute_names = winany.gamedat_attribute_names as Map<string, AttributeData>;
+export const gamedat_global_nums = winany.gamedat_global_nums as Map<number, GlobalData>;
+export const gamedat_global_names = winany.gamedat_global_names as Map<string, GlobalData>;
+export const gamedat_globals_sort_index = winany.gamedat_globals_sort_index as GlobalData[];
+export const gamedat_globals_sort_alpha = winany.gamedat_globals_sort_alpha as GlobalData[];
+export const gamedat_constant_names = winany.gamedat_constant_names as Map<string, ConstantData>;
+export const gamedat_object_ids = winany.gamedat_object_ids as Map<number, ObjectData>;
+export const gamedat_object_names = winany.gamedat_object_names as Map<string, ObjectData>;
+export const gamedat_object_room_ids = winany.gamedat_object_room_ids as Set<number>;
+export const gamedat_object_global_ids = winany.gamedat_object_global_ids as Set<number>;
+export const gamedat_object_treesort = winany.gamedat_object_treesort as Map<number, number>;
+export const gamedat_string_map = winany.gamedat_string_map as Map<number, StringData>;
+export const gamedat_dictword_addrs = winany.gamedat_dictword_addrs as Map<number, DictWordData>;
+export const gamedat_dictword_adjs = winany.gamedat_dictword_adjs as Map<number, DictWordData>;
+export const gamedat_preposition_nums = winany.gamedat_preposition_nums as Map<number, PrepositionData>;
+export const gamedat_grammar_verbnums = winany.gamedat_grammar_verbnums as Map<number, GrammarVerbData>;
+export const gamedat_grammar_lines = winany.gamedat_grammar_lines as GrammarLineData[];
+export const gamedat_grammar_line_addrs = winany.gamedat_grammar_line_addrs as Map<number, GrammarLineData>;
+export const gamedat_grammaractionlines = winany.gamedat_grammaractionlines as number[];
+export const gamedat_routine_addrs = winany.gamedat_routine_addrs as Map<number, RoutineData>;
+export const gamedat_routine_names = winany.gamedat_routine_names as Map<string, RoutineData>;
+export const gamedat_actions = winany.gamedat_actions as ActionData[];
+export const gamedat_sourcefiles = winany.gamedat_sourcefiles as SourceLinesMap;
+export const gamedat_distances = winany.gamedat_distances as AllDistanceMap;
+export const gamedat_roominfo_names = winany.gamedat_roominfo_names as Map<string, MapRoom>;
+export const gamedat_commentary = winany.gamedat_commentary as CommentaryMap;
+export const gamedat_commentarymap = winany.gamedat_commentarymap as CommentaryLineMap;
 
 let assetdir = 'visiterp';
-if ((window as any).visizork_options?.assetdir) {
-    assetdir = (window as any).visizork_options?.assetdir;
+if (winany.visizork_options?.assetdir) {
+    assetdir = winany.visizork_options?.assetdir;
 }
 
 export function getasset(filename?: string) : string
